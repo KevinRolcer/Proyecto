0532ea759b728d797d5f8237f93f95fd442874d1
KevinRolcer <kevinrolce6@gmail.com>
2024-10-10 08:51:48 -0600

    Eliminacion

--- servidor/borrar_usuario.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("conexion.php");
-if(!empty($_GET['id'])){
-    $clave=$_GET['id'];
-    $consulta=mysqli_query($conexion, "DELETE FROM usuarios WHERE idusu = $clave");
-    mysqli_close($conexion);
-    header('Location: ../cliente/Usuarios.php');    
-}
-?>
